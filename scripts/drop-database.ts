8afb110fdc9e1f54b326408e037942f3352441bf
Willis <Will.Willis@invesco.com>
2020-07-21 08:03:12 -0500

    recreate rights after dropping schema

--- scripts/drop-database.ts
+++ scripts/drop-database.ts
@@ -4,6 +4,10 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.executeRaw('DROP SCHEMA public CASCADE;');
+  //await prisma.executeRaw('CREATE SCHEMA public;');
+  await prisma.executeRaw('GRANT ALL ON SCHEMA public TO postgres;');
+  await prisma.executeRaw('GRANT ALL ON SCHEMA public TO public;');
+  await prisma.executeRaw("COMMENT ON SCHEMA public IS 'standard public schema';");
 }
 
 main();
